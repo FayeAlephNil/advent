b85be203beaf8c697b86e7fddb6de8486c1d5bf3
Striking <Strikingwolf2012@gmail.com>
2018-12-02 18:02:34 -0600

    Clean up on day2 part 2

--- 2018/Day2.hs
+++ 2018/Day2.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE TupleSections #-}
 module Main where
 
 import Data.Monoid
 import Data.Maybe
 
 main :: IO ()
-main = part get2
+main = putStrLn "Checksum:" >> part get1 >> putStrLn "\nCommon Box:" >> part get2
 
 part :: (Show a) => (String -> a) -> IO ()
 part f = fmap f (readFile "day2.in") >>= print
@@ -14,22 +13,17 @@ get1 :: String -> Int
 get1 = uncurry (*) . countNM 2 3
 
 get2 :: String -> Maybe String
-get2 s = do
-    (as, bs) <- getPair s
-    pure $ filterWithList (==) as bs
-
-filterWithList :: (a -> b -> Bool) -> [a] -> [b] -> [b]
-filterWithList f as bs = catMaybes $ zipWith (\a b -> if f a b then Just b else Nothing) as bs
-
-differByOne :: (Eq a) => [a] -> [a] -> Bool
-differByOne as bs = 1 == length (filterWithList (/=) as bs)
-
-getPair :: String -> Maybe (String, String)
-getPair s = listToMaybe $ do
-    l <- ls
-    (l,) <$> filter (differByOne l) ls
-    where 
+get2 s = listToMaybe $ do
+    l1 <- ls
+    l2 <- ls
+    case foldMap maybeMon $ maybeList l1 l2 of
+        (Sum 1, bs) -> pure bs
+        _ -> []
+    where
         ls = lines s
+        maybeList = zipWith (\a b -> if a == b then Just a else Nothing)
+        maybeMon Nothing = (Sum (1 :: Int), mempty)
+        maybeMon (Just a) = (mempty, [a])
 
 count :: Char -> String -> Int
 count c s = length $ filter (== c) s
@@ -42,6 +36,6 @@ hasN :: Int -> String -> Bool
 hasN n s = n `elem` fmap (($ s) . count) s
 
 countNM :: Int -> Int -> String -> (Int, Int)
-countNM n m s = case foldMap (\t -> (Sum $ boolInt $ hasN n t, Sum $ boolInt $ hasN m t)) $ lines s of 
+countNM n m s = case foldMap (\t -> (Sum $ boolInt $ hasN n t, Sum $ boolInt $ hasN m t)) $ lines s of
     (Sum a, Sum b) -> (a, b)
 
